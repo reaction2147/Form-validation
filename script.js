4853545c7aee9e7b298fa40b9928aa708101e383
William Ford <wford1.home@gmail.com>
2021-03-24 22:59:47 +0900

    Added live form validation

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+const email = document.getElementById("mail");
+if(!email.checkValidity()) {}
+const country = document.getElementById("country");
+if (!country.checkValidity()) {}
+
+
+};
+
